1efa89e6fd0b054712ac5158395b203c117b1bd0
Maksim Lin <maks@manichord.com>
2024-10-11 21:06:26 +1100

    tweak settings, use 2op fm, start naming registers

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include "utils.h"
 #include <opal.h>
 
-const int SAMPLE_RATE = 44100;
-const int BUFFER_SIZE = 512;
+#define SAMPLE_RATE 44100
+#define BUFFER_SIZE 512
+
+#define FREQ_BASE_REG 0xA0
+#define OCTAVE_BASE_REG 0xB0
 
 int8_t *audio_buffer;
 
@@ -27,13 +30,12 @@ void oscillator_callback(void *userdata, uint8_t *byteStream,
     int16_t l, r;
     opl_.Sample(&l, &r);
 
-    // *2 to bump up output volume for now
-    s_byte_stream[i++] = l * 2;
-    s_byte_stream[i++] = r * 2;
+    s_byte_stream[i++] = l;
+    s_byte_stream[i++] = r;
   }
 }
 
-const int startMidiNote = 69;
+const int startMidiNote = 60;
 
 static const unsigned int noteFNumbers[] = {342, 363, 385, 408, 432, 458,
                                             485, 514, 544, 577, 611, 647};
@@ -67,20 +69,26 @@ static void handle_note_keys(SDL_Keysym *keysym) {
   case SDLK_k:
     new_note = startMidiNote + 7;
     break;
+  case SDLK_l:
+    new_note = startMidiNote + 8;
+    break;
+  default:
+    new_note = 0;
+    break;
   }
 
   // set note in OPAL
   u_char block = new_note / 12;
   u_int16_t fnum = noteFNumbers[new_note % 12];
-  printf("PLAY block[%d] fnum:%d\n", block, fnum);
+  printf("PLAY %d block[%d] fnum:%d\n", new_note, block, fnum);
 
   u_char areg = fnum & 0xFF; // truncate to lowest 8 bytes
   //        note on  block in d2,3,4  high 2 bits of fnum in D0,D1
   u_char breg = 0x20 | (block << 2) | (fnum >> 8);
 
   // Note on with the fnumber that matches the give midi note
-  opl_.Port(0xA0, areg);
-  opl_.Port(0xB0, breg);
+  opl_.Port(FREQ_BASE_REG, areg);
+  opl_.Port(OCTAVE_BASE_REG, breg);
 
   // printBits(sizeof(fnum), &fnum);
   // printBits(sizeof(areg), &areg);
@@ -91,39 +99,37 @@ static void handle_key_down(SDL_Keysym *keysym) { handle_note_keys(keysym); }
 
 int main(int argc, char const *argv[]) {
 
-  // Frequency
-  opl_.Port(0xA0, 0xe7);
-  // Note on, block, hi freq
-  opl_.Port(0xB0, 0x31);
+  opl_.Port(0x01, 0x20);
+
   // Tremolo/Vibrato/Sustain/KSR/Multiplication
-  opl_.Port(0x20, 0x00);
+  opl_.Port(0x20, 0x01);
   opl_.Port(0x23, 0x01);
-  opl_.Port(0x28, 0x06);
-  opl_.Port(0x2B, 0x01);
+  opl_.Port(0x28, 0x00);
+  opl_.Port(0x2B, 0x00);
   // Waveform
-  opl_.Port(0xE0, 0x01);
-  opl_.Port(0xE3, 0x01);
+  opl_.Port(0xE0, 0x00); // 0 = pure sine
+  opl_.Port(0xE3, 0x00);
   opl_.Port(0xE8, 0x02);
   opl_.Port(0xEB, 0x02);
   // Key Scale Level/Output Level
-  opl_.Port(0x40, 0x40);
-  opl_.Port(0x43, 0x1c);
+  opl_.Port(0x40, 0x0F);
+  opl_.Port(0x43, 0x00);
   opl_.Port(0x48, 0x5c);
   opl_.Port(0x4B, 0x02);
   // Attack Rate/Decay Rate
-  opl_.Port(0x60, 0x72);
-  opl_.Port(0x63, 0x71);
+  opl_.Port(0x60, 0xf0);
+  opl_.Port(0x63, 0xf0);
   opl_.Port(0x68, 0x82);
   opl_.Port(0x6B, 0xf0);
   // Sustain Level/Release Rate
-  opl_.Port(0x80, 0x86);
-  opl_.Port(0x83, 0x94);
+  opl_.Port(0x80, 0xff);
+  opl_.Port(0x83, 0xff);
   opl_.Port(0x88, 0xbc);
   opl_.Port(0x8B, 0xbc);
 
   // enable left/right only for channel0
-  opl_.Port(0xc0, 0x37);
-  opl_.Port(0xc1, 0x00);
+  opl_.Port(0xc0, 0x31);
+  opl_.Port(0xc1, 0x30);
   opl_.Port(0xc2, 0x00);
   opl_.Port(0xc3, 0x00);
   opl_.Port(0xc4, 0x00);
